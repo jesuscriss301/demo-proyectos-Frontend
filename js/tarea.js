332122959b4470cd58e33593959b94d87c159db2
jesus <jesuscriss301@gmail.com>
2023-01-30 15:07:38 -0500

    tareas.json

--- js/tarea.js
+++ js/tarea.js
@@ -4,29 +4,30 @@ jsonproyecto=JSON.parse(a);
 proyecto= localStorage.getItem("proyecto");
 cargaProyectos()
 
-var url ="https://raw.githubusercontent.com/jesuscriss301/demo-proyectos-Frontend/main/json/proyecto.json";
+var url ="https://raw.githubusercontent.com/jesuscriss301/demo-proyectos-Frontend/main/json/tarea.json";
 fetch(url)
 .then((response) => response.json())
-.then((json) => cargaProyectos(json));
+.then((json) => cargaTareas(json));
 
-function cargaProyectos(json) {
-let a=localStorage.getItem("proyectonuevo");
+function cargaTareas(json) {
+let a=localStorage.getItem("tareas");
 
-if(a!=""){
-proyectojson=JSON.parse(a);
+if(a!=null){
+    tareajson=JSON.parse(a);
 }else{
-proyectojson =json;
+    tareajson =json;
+    console.log(tareajson);
 }
-let tblBody= document.querySelector("#proyectos");
-let proyecto= document.querySelector("#proyecto");
+let tblBody= document.querySelector("#tareas");
+let tarea= document.querySelector("#tarea");
 let table = document.querySelector("#table");
 let actualizar= document.querySelector("#actualizar");
 let eliminar= document.querySelector("#eliminar");
 let ver= document.querySelector("#ver");
 tblBody.innerHTML="";
-for (var i = 0; i < proyectojson.length; i++) {
-    
+for (var i = 0; i < tareajson.length; i++) {
     // Crea las hileras de la tabla
+    if (true) {
     var hilera = document.createElement("tr");
     
       var celda = document.createElement("td");
@@ -34,29 +35,30 @@ for (var i = 0; i < proyectojson.length; i++) {
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
       var celda = document.createElement("td");
-      var textoCelda = document.createTextNode(JSON.stringify( proyectojson[i].idProyecto));
+      var textoCelda = document.createTextNode(tareajson[i].idTarea.toString());
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
       var celda = document.createElement("td");
-      var textoCelda = document.createTextNode(fecha());
+      var textoCelda = document.createTextNode(JSON.stringify(tareajson[i].nombreTarea));
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
       var celda = document.createElement("td");
-      var textoCelda = document.createTextNode(proyectojson[i].nombreProyecto.toString());
+      var textoCelda = document.createTextNode(JSON.stringify(tareajson[i].descripcionTarea));
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
       var celda = document.createElement("td");
-      var textoCelda = document.createTextNode(proyectojson[i].descripcionProyecto.toString());
+      var textoCelda = document.createTextNode(JSON.stringify(tareajson[i].fecha));
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
       var celda = document.createElement("td");
-      var textoCelda = document.createTextNode("0%");
+      var textoCelda = document.createTextNode(JSON.stringify(tareajson[i].etapa));
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
       var celda = document.createElement("td");
-      var textoCelda = document.createTextNode("prueba");
+      var textoCelda = document.createTextNode(JSON.stringify(tareajson[i].completado));
       celda.appendChild(textoCelda);
       hilera.appendChild(celda);
+      
       var celda = document.createElement("td");
 
       var newactualizar=actualizar.cloneNode(true);
@@ -74,12 +76,13 @@ for (var i = 0; i < proyectojson.length; i++) {
 
     // agrega la hilera al final de la tabla (al final del elemento tblbody)
     tblBody.appendChild(hilera);
-  }
+  }}
   
   table.appendChild(tblBody);
-  localStorage.setItem("proyectonuevo",JSON.stringify(proyectojson));
-  proyecto.remove();
-}
+  //localStorage.setItem("proyectonuevo",JSON.stringify(proyectojson));
+  tarea.remove();
+    }
+
 function cargaProyectos(){
   //  if(localStorage.getItem("nuevoProyecto")=='['){
     jsonproyecto= jsonproyecto[proyecto];
